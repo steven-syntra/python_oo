12d2dbc17ea688603e35b5d5085ed231eb8f09e4
Steven De Ryck <steven@inform.be>
2022-02-16 20:55:08 +0100

    Versie 2

--- Dier.py
+++ Dier.py
@@ -1,29 +1,38 @@
 class Dier:
     naam = ""
+    soort = ""      # hond, kat, kabeljauw
     poten = 0
     voedsel = []
     geluid = ""
     huid = ""
 
+    # constructor
+    def __init__(self, naam_dier, soort_dier):
+        self.naam = naam_dier
+        self.soort = soort_dier
 
-cat = Dier()
-cat.naam = "zini"
+    def beknopte_info(self):
+        print(f"{self.naam}, {self.geluid}, {self.huid}")
+
+    def uitgebreide_info(self):
+        print(f"{self.naam}, {self.geluid}, {self.huid}")
+        print("Voedsel: ")
+        print(", ".join(self.voedsel))
+        print("===========")
+        print("")
+
+
+cat = Dier("zini", "kat")
 cat.poten = 4
-cat.voedsel = [ "muizen", "paté", "vogels", "insecten" ]
+cat.voedsel = ["muizen", "paté", "vogels", "insecten"]
 cat.geluid = "miauw"
 cat.huid = "vlekkenpels"
 
-hond = Dier()
-hond.naam = "lotje"
+hond = Dier("lotje", "hond")
 hond.poten = 4
-hond.voedsel = [ "brokken" ,"vlees", "schoenen" ]
+hond.voedsel = ["brokken", "vlees", "schoenen"]
 hond.geluid = "woef"
 hond.huid = "bruine pels"
 
-print( f"{cat.naam}, {cat.geluid}, {cat.huid}")
-print( f"{hond.naam}, {hond.geluid}, {hond.huid}")
-
-
-
-
-
+cat.uitgebreide_info()
+hond.uitgebreide_info()
