3d8c730c54af9fca010ab34971b5c9629dc3eabe
steven-syntra <info@inform.be>
2022-05-02 21:32:44 +0200

    Pandas_MySQL, met LIKE erbij

--- Pandas_Mysql/Ex1_6.py
+++ Pandas_Mysql/Ex1_6.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 my_conn = 'mysql://root:@localhost/sakila'
-films = pd.read_sql("SELECT * FROM film", my_conn)
+films = pd.read_sql("SELECT * FROM film WHERE title LIKE '%%ARK%%'", my_conn)
 
 print(films[['title', 'release_year', 'length']])
